b88d87131a76da55ec2142e0987d8afd044cb52a
dzj <thudzj@qq.com>
2016-06-01 11:34:53 +0800

    Add icon

--- SOA_Music/index.php
+++ SOA_Music/index.php
@@ -13,7 +13,7 @@ $code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-		<link rel="shortcut icon" href="assets/img/favicon.png">
+		<link rel="shortcut icon" href="images/favicon.ico">
     <title>Musicooon</title>
 		
 		 <!-- Bootstrap -->
